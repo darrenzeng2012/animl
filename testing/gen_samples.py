449d6f1d4204843290358f5cb04a58bf621a9d5d
parrt <parrt@cs.usfca.edu>
2018-09-20 10:57:32 -0700

    always generate regression leaves even if non-fancy

--- testing/gen_samples.py
+++ testing/gen_samples.py
@@ -237,7 +237,7 @@ if __name__ == '__main__':
     print(f"tmp dir is {tempfile.gettempdir()}")
     for f in viz_funcs:
         name = f.__name__[len("viz_"):]
-        if name!='boston': continue
+        # if name!='boston': continue
         save(name, dirname, "TD", 2)
         save(name, dirname, "TD", 4)
         if name=='iris':
